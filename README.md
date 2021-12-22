e414f352283c0e7f2a7f79090c3247849177be29
Mohammad Mustakim Ali <mustakimali@users.noreply.github.com>
2021-12-22 15:57:29 +0000

    MVP that works

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
 # Spotfy Buddy
 
-Find and recommend unique songs you've never listened before.
+Find and recommend unique songs you've never listened before based on your favorite songs in Spotify library.
+
+## Demo
+```bash
+# export the access token (get it from spotify dev portal with scopes: user-read-private, user-library-read, playlist-modify-public, playlist-modify-private)
+$ export access_token="...."
+
+# Download you library to use as a seed to recommend new song
+# saves as spo_library.tsv (by default)
+$ ./spotify.py library
+
+# Download all other playlists (so we know what to exclude from the recommendataion)
+# saved as spo_all_playlists.tsv
+$ ./spotify.py list-playlists | awk '{print $1}' | xargs -I{} ./spotify.py get-playlist --id={} > spo_all_playlists.tsv
+
+# Generate recommendation
+# reads from spo_library.tsv (by default, param: --input)
+# saved as spo_recs_all.tsv (by default, param: --output)
+$ ./spotify.py recommend --exclude=spo_all_playlists.tsv
+
+# create playlist with the recommendataion
+# reads from spo_recs_all.tsv (by default, param: --input)
+$ ./spotify.py create-playlist --name={NAME}
+
+# check your spotify
+```
+
+## Get all playlists
+Useful to use as exclude list in recommendation
+
+```bash
+$ export access_token="...."
+$ ./spotify.py list-playlists | awk '{print $1}' | xargs -I{} ./spotify.py get-playlist --id={} > spo_all_playlists.tsv
+```
--- spotify.py
+++ spotify.py
@@ -1,53 +1,405 @@
 #!/usr/bin/env /usr/bin/python3
 
 import os
+import sys
 import requests
 import json
+import argparse
+from datetime import datetime
+import sys
+
+# Scopes: user-read-private, user-library-read, playlist-modify-public, playlist-modify-private
+#
+# https://developer.spotify.com/documentation/web-api/reference/#/operations/get-recommendations
+# https://developer.spotify.com/console/library/
 
 # ------------------------------------------------------------
 def delete_file(fpath):
     if os.path.exists(fpath):
         os.remove(fpath)
-# ------------------------------------------------------------
-FILE_NAME_LIBRARY = 'spo_library.txt'
-FILE_NAME_LIBRARY_RAW = 'spo_library_raw.txt'
-
-ACCESS_TOKEN = os.getenv("access_token")
-if ACCESS_TOKEN == None or ACCESS_TOKEN == "":
-    print("Must set access_token env variable")
-    exit(1)
-
-delete_file(FILE_NAME_LIBRARY)
-delete_file(FILE_NAME_LIBRARY_RAW)
 
 
-library_file = open(FILE_NAME_LIBRARY, 'a')
-library_file_raw = open(FILE_NAME_LIBRARY_RAW, 'a')
-offset = 0
-while True:
-    rsp = requests.get(f'https://api.spotify.com/v1/me/tracks?limit=50&offset={offset}', headers={
-        'Authorization': f'Bearer {ACCESS_TOKEN}'
-    })
-    if rsp.status_code != 200:
-        print(f"Invalid status code from spotify: {rsp.status_code}\n{rsp.text}")
-        exit(2)
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
+
+def ensure_exist(fpath):
+    if os.path.exists(fpath) == False:
+        eprint(f"File does not exist: {fpath}")
+        exit(3)
+
+
+def get_access_token():
+    token = os.getenv("access_token")
+    if token == None or token == "":
+        eprint("Must set access_token env variable")
+        exit(1)
+    return token
+
+
+def spotify_api_call(path, params=None):
+    access_token = get_access_token()
+
+    path = f"https://api.spotify.com/{path}"
+    rsp = None
+
+    if params == None:
+        # get
+        rsp = requests.get(
+            path,
+            headers={"Authorization": f"Bearer {access_token}"},
+        )
     else:
+        # post
+        rsp = requests.post(
+            path, json=params, headers={"Authorization": f"Bearer {access_token}"}
+        )
+
+    if rsp.status_code >= 300:
+        eprint(
+            f"Invalid status code from spotify [{path}]: {rsp.status_code}\n{rsp.text}"
+        )
+        return None, None
+
+    return (json.loads(rsp.text), rsp.text)
+
+
+# ------------------------------------------------------------
+
+
+def handle_load_recommendataion(infile, outfile, exclude=None):
+    def load_file(fpath, dest_hashmap, dest_ids=None):
+        f = open(fpath, "r")
+
+        titles = []
+        ids = []
+        for line in f.readlines():
+            if line.startswith("#"):
+                continue
+
+            parts = line.split("\t")
+            if len(parts) < 2:
+                continue
+            id = parts[0]
+            title = parts[1].strip()
+            titles.append(title)
+            ids.append(id)
+
+        f.close()
+
+        print(f"Loading {len(ids)} tracks from {fpath}...")
+        for i in range(0, len(ids)):
+            id = ids[i]
+            title = titles[i]
+
+            dest_hashmap[id] = title
+            dest_hashmap[title] = id  # make sure song with same title is excluded
+            if dest_ids != None:
+                dest_ids.append(id)
+
+    print(f"Generating recommendations from {infile} to {outfile}")
+    # load library
+    if os.path.exists(infile) == False:
+        handle_load_library(infile)
+
+    # load library into memory
+    exclude_tracks_hm = (
+        {}
+    )  # hashmap for quick lookup if a track is already loaded, library is already included
+    seed_ids = []  # the ids used as seed for recommendataion
+
+    # load seeds
+    load_file(infile, exclude_tracks_hm, seed_ids)
+    exclude_ids = seed_ids
+
+    # load exclude lists (if any)
+    if exclude != None:
+        paths = exclude.split(",")
+        for path in paths:
+            load_file(path, exclude_tracks_hm, None)  # None - to not used them as seed
+
+    print(
+        f"Generating recommendation based on {len(seed_ids)}s and {len(exclude_tracks_hm)} excluded tracks..."
+    )
+    # download recommendataion
+    delete_file(outfile)
+    rec_file = open(outfile, "a")
+    # header
+    rec_file.write("# id\ttitle\tartist_name\talbum_name\tpopularity\n")
+    i = 0
+    while i < len(seed_ids):
+        print(f"Loading recommmendations {i}/{len(seed_ids)}...", end="")
+
+        ids = ",".join(map(lambda x: (x), seed_ids[i : i + 5]))
+
+        # get recommendation
+        (resp, raw) = spotify_api_call(
+            f"v1/recommendations?limit=100&seed_tracks={ids}"
+        )
+        if resp == None:
+            continue
+
+        tracks = resp["tracks"]
+        num_added = 0
+        num_skipped = 0
+        for item in tracks:
+            id = item["id"]
+            title = item["name"]
+            album_name = item["album"]["name"]
+            artist_name = item["artists"][0]["name"]
+            popularity = item["popularity"]
+
+            if exclude_tracks_hm.__contains__(id) == False:
+                num_added += 1
+                rec_file.write(
+                    f"{id}\t{title}\t{artist_name}\t{album_name}\t{popularity}\n"
+                )
+                exclude_tracks_hm[id] = title
+            else:
+                num_skipped += 1
+
+        print(f": {num_added} added, {num_skipped} skipped.")
+
+        i += 5
+
+    rec_file.close()
+    print("Sorting")
+
+    os.system(
+        f"cat {outfile} | (sed -u 1q; sort -nrk 5 -t '\t') > {outfile}_sorted.tsv"
+    )
+    os.system(f"mv {outfile}_sorted.tsv {outfile}")
+
+    print("Done")
+
+
+def handle_load_library(ofile):
+
+    delete_file(ofile)
+
+    library_file = open(ofile, "a")
+    # header
+    library_file.write("# id\ttitle\tartist_name\talbum_name\n")
+    offset = 0
+
+    while True:
+        print(f"Downloading library (offset: {offset})")
+        (resp, raw) = spotify_api_call(f"v1/me/tracks?limit=50&offset={offset}")
+        if resp == None:
+            break
+
+        tracks = resp["items"]
+
+        if len(tracks) == 0:
+            break
+
+        for item in tracks:
+            track = item["track"]
+            id = track["id"]
+            title = track["name"]
+            album_name = track["album"]["name"]
+            artist_name = track["artists"][0]["name"]
+
+            library_file.write(f"{id}\t{title}\t{artist_name}\t{album_name}\n")
+
         offset += 50
-    
-    #print(rsp.text)
 
-    json = json.loads(rsp.text)
-    
-    for item in json['items']:
-        item = item['track']
-        id = item['id']
-        title = item['name']
+    library_file.close()
 
-        
-        print(f'#{id} {title}')
-        library_file.write(f'{id}\t{title}\n')
-        library_file_raw.write(rsp.text)
-    break
 
-library_file.close()
-library_file_raw.close()
+def handle_create_playlist(infpath, playlistname):
+
+    # read input
+    infile = open(infpath, "r")
+    trackIds = []
+    for line in infile.readlines():
+        if line.startswith("#"):
+            continue
+
+        parts = line.split("\t")
+        id = f"spotify:track:{parts[0]}"
+        trackIds.append(id)
+    infile.close()
+    total_tracks = len(trackIds)
+    confirm = input(
+        f'Confirm you want to add {total_tracks} songs to a new playlist "{playlistname}" (y/n): '
+    )
+
+    if confirm != "y":
+        print(f'You pressed "{confirm}", Aborting')
+        return
+    # get user id
+    (resp, raw) = spotify_api_call("v1/me")
+    if resp == None:
+        return
+    user_id = resp["id"]
+
+    # create playlist
+    dt_string = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    dt_string_short = datetime.now().strftime("%y%m%d%H%M")
+    playlistname = f"{playlistname} by spotify-buddy @ {dt_string_short}"
+    print(f"Creating playlist {playlistname}")
+    (resp, raw) = spotify_api_call(
+        f"v1/users/{user_id}/playlists",
+        {
+            "name": playlistname,
+            "public": False,
+            "description": f"Created by spotify-buddy on {dt_string}",
+        },
+    )
+    if resp == None:
+        return
+
+    playlist_id = resp["id"]
+
+    # add tracks
+    for i in range(0, len(trackIds), 100):
+        print(f"Adding tracks {i}/{len(trackIds)}")
+        (resp, raw) = spotify_api_call(
+            f"v1/playlists/{playlist_id}/tracks", {"uris": trackIds[i : i + 100]}
+        )
+        if resp == None:
+            return
+        i += 100
+
+    print(f"Done")
+
+
+def handle_list_playlist():
+    offset = 0
+    while True:
+        (resp, raw) = spotify_api_call(f"v1/me/playlists?limit=50&offset={offset}")
+        if resp == None or len(resp["items"]) == 0:
+            return
+
+        for item in resp["items"]:
+            id = item["id"]
+            title = item["name"]
+
+            print(f"{id}\t{title}")
+        offset += 50
+
+
+def handle_get_playlist(playlist_id):
+    if playlist_id.__contains__("\t"):
+        # handle when the output of list-playlists is piped
+        playlist_id = playlist_id.split("\t")[0]
+
+    (resp, _) = spotify_api_call(f"v1/playlists/{playlist_id}?fields=name")
+    if resp == None:
+        eprint(f"Error getting playlist with id: {playlist_id}")
+        exit(5)
+    title = resp["name"]
+    print(f"# Playlist: {playlist_id}\t{title}")
+    print("# id\ttitle\tartist_name\talbum_name\tpopularity\n")
+
+    offset = 0
+    while True:
+        fields = (
+            "fields=items(track(id%2Cname%2Cpopularity%2Calbum(name)%2Cartists(name)))"
+        )
+        (resp, _) = spotify_api_call(
+            f"v1/playlists/{playlist_id}/tracks?limit=50&offset={offset}&fields={fields}"
+        )
+        if resp == None or len(resp["items"]) == 0:
+            return
+
+        for item in resp["items"]:
+            track = item["track"]
+            if track == None:
+                break
+
+            id = track["id"]
+            if id == None:
+                continue
+
+            title = track["name"]
+            album_name = track["album"]["name"]
+            artist_name = track["artists"][0]["name"]
+            popularity = track["popularity"]
+
+            print(f"{id}\t{title}\t{artist_name}\t{album_name}\t{popularity}")
+        offset += 50
+
+
+def main():
+    FILE_NAME_LIBRARY = "spo_library.tsv"
+    FILE_NAME_RECOMMENDATAION = "spo_recs_all.tsv"
+
+    parser = argparse.ArgumentParser(description="Recommend new songs")
+    cmd = parser.add_subparsers(dest="command")
+
+    # download playlist/library
+    library = cmd.add_parser("library", help="load library")
+    library.add_argument(
+        "--out",
+        help=f"Outfile file path [optional, default {FILE_NAME_LIBRARY}]",
+        default=FILE_NAME_LIBRARY,
+    )
+
+    # recommend songs based on seed (--input, a tsv file)
+    recommend = cmd.add_parser("recommend", help="generate recommendations")
+    recommend.add_argument(
+        "--input",
+        help=f"Generate recommendations based on tracks in file path [optional, default {FILE_NAME_LIBRARY}]",
+        default=FILE_NAME_LIBRARY,
+    )
+    recommend.add_argument(
+        "--out",
+        help=f"Outfile file path [optional, default {FILE_NAME_RECOMMENDATAION}]",
+        default=FILE_NAME_RECOMMENDATAION,
+    )
+    recommend.add_argument(
+        "--exclude",
+        help=f"Comma separated files with tracks to exclude in this mix [optional]",
+    )
+
+    # create playlist from recommendations (--input, a tsv file, --name, name)
+    cplaylist = cmd.add_parser(
+        "create-playlist",
+        help="create playlist from a generated recommendations file (*.tsv)",
+    )
+    cplaylist.add_argument(
+        "--input",
+        help=f"The file containing all songs details (default: {FILE_NAME_RECOMMENDATAION})",
+        default=FILE_NAME_RECOMMENDATAION,
+    )
+    cplaylist.add_argument("--name", help=f"Name of the playlist", required=True)
+
+    # basic command to talk to spotify
+    # list playlists in stdout
+    getallplaylists = cmd.add_parser("list-playlists", help="List all user playlists")
+    # download playlists
+    downloadplaylist = cmd.add_parser("get-playlist", help="Get a playlist")
+    downloadplaylist.add_argument(
+        "--id", help=f"id of the playlist to dowload", required=True
+    )
+    downloadplaylist.add_argument(
+        "--out",
+        help=f"Output file to write (use stdout if not specified)",
+    )
+
+    args = parser.parse_args()
+
+    if args.command == None:
+        parser.print_help()
+        exit(3)
+
+    _ = get_access_token()  # validate access token
+
+    if args.command == "library":
+        handle_load_library(args.out)
+    elif args.command == "recommend":
+        handle_load_recommendataion(args.input, args.out, args.exclude)
+    elif args.command == "create-playlist":
+        handle_create_playlist(args.input, args.name)
+    elif args.command == "list-playlists":
+        handle_list_playlist()
+    elif args.command == "get-playlist":
+        handle_get_playlist(args.id)
+    else:
+        parser.print_help()
+
+
+if __name__ == "__main__":
+    main()
